70ce6d1eaabbc447a49774b4e8294dbd7f623211
Varun Gupta <varungp3@gmail.com>
2026-05-02 10:03:47 -0700

    Implement Cross-entropy loss (Math Foundations 4/6)

---
+++ foundations/loss.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: true labels (0 or 1)
+        # y_pred: predicted probabilities
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        # np.assert(y_true.shape == y_pred.shape)
+        eps = 1e-7
+        y_pred = np.minimum(1, np.maximum(eps, y_pred))
+        return np.round(-np.sum((y_true==1) * np.log(y_pred) + (y_true == 0) * np.log(1.0 - y_pred)) / len(y_true), 4)
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
+        # y_pred: predicted probabilities (shape: n_samples x n_classes)
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        # np.assert(y_true.shape == y_pred.shape)
+        eps = 1e-7
+        y_pred = np.minimum(1, np.maximum(eps, y_pred))
+        print("y_true: ", y_true)
+        print("y_pred: ", y_pred)
+        print((y_true==1) * np.log(y_pred))
+        print((y_true == 0) * np.log(1.0 - y_pred))
+        print(y_true.shape[0])
+        return np.round(-np.sum(np.sum((y_true==1) * np.log(y_pred))) / y_true.shape[0], 4)
